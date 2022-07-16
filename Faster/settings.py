5f0dc9672f1e608b2649c1598e7355e9b34eb107
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-07-16 13:25:13 +0530

    Update settings.py

--- Faster/settings.py
+++ Faster/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
- 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+MEDIA_DIR = BASE_DIR / 'media'
+MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
