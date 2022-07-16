308caaca66f44c3996e7327d39102d840e9017fb
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-07-16 13:15:00 +0530

    Update settings.py

--- Faster/settings.py
+++ Faster/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.1.12', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
