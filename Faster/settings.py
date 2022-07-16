f263398aa11760f959e0409e3f2656face188cd8
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-07-16 13:35:15 +0530

    Update settings.py

--- Faster/settings.py
+++ Faster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^S&^D*&^D*&SA^*D^*&S&*fhdskjghknfdkhdsfnuhskfh@#$@#$@#$@#$@#$@#4jh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.12', '127.0.0.1','fastercomputers.herokuapp.com/']
+ALLOWED_HOSTS = ['192.168.1.12', '127.0.0.1','fastercomputers.herokuapp.com','localhost','*']
 
 
 # Application definition
